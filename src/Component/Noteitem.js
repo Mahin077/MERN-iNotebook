cd52df8cce6b9f621e2b50d0329fe3f9026d9ec8
Mahin077 <mmahinm10@gmail.com>
2023-08-13 22:29:57 +0600

    font awesome has been used for icons. Alert comonent has been created

--- src/Component/Noteitem.js
+++ src/Component/Noteitem.js
@@ -3,11 +3,17 @@ import React from 'react'
 const Noteitem = (props) => {
     const { note } = props;
     return (
-        <div className="col-md-3"> 
-            <div class="card my-3"> 
-                <div class ="card-body">
-                <h5 class ="card-title">{note.title}</h5>
-                <p class ="card-text">{note.description}</p> 
+        <div className="col-md-3">
+            <div className="card my-3">
+                <div className="card-body">
+                    <div className="d-flex">
+                        <h5 className="card-title me-auto">{note.title}</h5>
+                        <i className="fa-solid fa-trash-can mx-1"></i>
+                        <i className="fa-regular fa-pen-to-square mx-1"></i>
+                    </div>
+
+                    <p className="card-text">{note.description}</p>
+
                 </div>
             </div>
         </div>
